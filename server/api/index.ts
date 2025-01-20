f43b008545dfd83ab2480db38bb9f4dd490c0fce
ilushacomeback <ilya.rudenko.2105@mail.ru>
2025-01-20 21:52:25 +0300

    fix server/api

--- server/api/index.ts
+++ server/api/index.ts
@@ -4,26 +4,29 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const router = require('../router/index');
-const errorMiddleware = require('../middlewares/error-middleware')
+const errorMiddleware = require('../middlewares/error-middleware');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-
 
 app.use(cors({ origin: 'https://test-task-java-code-drab.vercel.app', credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use('/api', router);
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-const start = () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`listen: ${PORT}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
+if (process.env.NODE_ENV !== 'production') {
+  const start = () => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+      });
+    } catch (e) {
+      console.error('Error starting server:', e);
+    }
+  };
 
-start();
+  start();
+}
+
+module.exports = app;
